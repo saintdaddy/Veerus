ebfce30287be0cbdb3aa31bd5e9c8e51ac082a80
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-19 21:28:53 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -16,4 +16,4 @@ val = input("Do you use py, python or python3 filename.py to launch file? (py/py
 
 os.system(val + " hash.py")
 
-os.system(val + ' -m PiInstaller --onefile client.py.hashed.py -i "NONE"')
+os.system(val + ' -m PyInstaller --onefile client.py.hashed.py -i "NONE"')
